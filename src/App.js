0c7a1949d9d340f3c8700b7fb5ab3535f303fbbc
cath974 <60576134+cath974@users.noreply.github.com>
2020-04-30 11:16:20 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import Home from './components/Home';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import React from "react";
+import Home from "./components/Home";
+import About from './components/About';
 
-function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
-}
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+const App = () => (
+  
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+    </Switch>
+
+)
 
 export default App;
